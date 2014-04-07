11b045f09b087f47dc884637d07798ab61a851ad
harprit singh <harprit.singh@itaas.com>
2014-04-07 18:32:11 +0530

    Added wrapper reset focus.

--- navigation.js
+++ navigation.js
@@ -9,43 +9,19 @@ window.onload = function() {
 };
 
 keyhandler.handleUp = function () {
-  var lastFocus = focusedElm;
-  var tmp = iterators.getUpFocusable(focusedElm);
-  if(tmp){
-    focusedElm = tmp;
-    unsetFocus(lastFocus);
-    setFocus(focusedElm);
-  }
+ resetFocus(iterators.getUpFocusable(focusedElm));
 };
 
 keyhandler.handleDown = function () {
-  var lastFocus = focusedElm;
-  var tmp = iterators.getDownFocusable(focusedElm);
-  if(tmp){
-    focusedElm = tmp;
-    unsetFocus(lastFocus);
-    setFocus(focusedElm);
-  }
+  resetFocus(iterators.getDownFocusable(focusedElm));
 };
 
 keyhandler.handleLeft = function () {
-  var lastFocus = focusedElm;
-  var tmp = iterators.getLeftFocusable(focusedElm);
-  if(tmp){
-    focusedElm = tmp;
-    unsetFocus(lastFocus);
-    setFocus(focusedElm);
-  }
+  resetFocus(iterators.getLeftFocusable(focusedElm));
 };
 
 keyhandler.handleRight = function () {
-  var lastFocus = focusedElm;
-  var tmp = iterators.getRightFocusable(focusedElm);
-  if(tmp){
-    focusedElm = tmp;
-    unsetFocus(lastFocus);
-    setFocus(focusedElm);
-  }
+  resetFocus(iterators.getRightFocusable(focusedElm));
 };
 
 keyhandler.register = function () {
@@ -79,6 +55,163 @@ function init () {
   keyhandler.register();
 }
 
+iterators.getUpFocusable = function (elm) {
+  var parent;
+  if(!elm){
+    return false;
+  }else if(elm.getAttribute('data-nav') === 'top-container'){
+    return false;
+  }
+  parent = elm.parentNode;
+  if(parent){
+    if(parent.getAttribute('data-nav') === 'vertical-container'){
+      return iterators.getPrevFocusableSibling(elm);
+    }else if(parent.getAttribute('data-nav') === 'top-container'){
+      return iterators.getPrevFocusableSibling(elm);
+    }else{
+      return iterators.getUpFocusable(parent);
+    }
+  }
+  return false;
+};
+
+iterators.getDownFocusable = function (elm) {
+  var parent;
+  if(!elm){
+    return false;
+  }else if(elm.getAttribute('data-nav') === 'top-container'){
+    return false;
+  }
+  parent = elm.parentNode;
+  if(parent){
+    if(parent.getAttribute('data-nav') === 'vertical-container'){
+      return iterators.getNextFocusableSibling(elm);
+    }else if(parent.getAttribute('data-nav') === 'top-container'){
+      return iterators.getNextFocusableSibling(elm);
+    }else{
+      return iterators.getDownFocusable(parent);
+    }
+  }
+  return false;
+};
+
+iterators.getRightFocusable = function (elm) {
+  var parent;
+  if(!elm){
+    return false;
+  }else if(elm.getAttribute('data-nav') === 'top-container'){
+    return false;
+  }
+  parent=elm.parentNode;
+  if(parent){
+    if(parent.getAttribute('data-nav') === 'horizontal-container'){
+      return iterators.getNextFocusableSibling(elm);
+    }else if(parent.getAttribute('data-nav') === 'top-container'){
+      return iterators.getNextFocusableSibling(elm);
+    }else{
+      return iterators.getRightFocusable(parent);
+    }
+  }
+  return false;
+};
+
+iterators.getLeftFocusable = function (elm) {
+  var parent;
+  if(!elm){
+    return false;
+  }else if(elm.getAttribute('data-nav') === 'top-container'){
+    return false;
+  }
+  parent=elm.parentNode;
+  if(parent){
+    if(parent.getAttribute('data-nav') === 'horizontal-container'){
+      return iterators.getPrevFocusableSibling(elm);
+    }else if(parent.getAttribute('data-nav') === 'top-container'){
+      return iterators.getPrevFocusableSibling(elm);
+    }else{
+      return iterators.getLeftFocusable(parent);
+    }
+  }
+  return false;
+};
+
+iterators.getFirstFocusable = function (elm) {
+  var all;
+  if(!elm){
+    return false;
+  }
+  if(elm && elm.getAttribute("data-nav") === 'focusable'){
+    return elm;
+  }
+  all = elm.getElementsByTagName("*");
+  for (var i=0, max=all.length; i < max; i++) {
+    if(all[i].getAttribute("data-nav") === 'focusable'){
+      return all[i];
+    }
+  }
+  return false;
+};
+
+iterators.getNextFocusableSibling = function (elm) {
+  var nxtSibling;
+  if(!elm){
+    return false;
+  }
+  nxtSibling = iterators.getNextSibling(elm);
+  if(nxtSibling ){
+    var focusable = iterators.getFirstFocusable(nxtSibling);
+    if(focusable){
+      return focusable;
+    }else{
+      return iterators.getNextFocusableSibling(nxtSibling);
+    }
+  }
+  return false;
+};
+
+iterators.getPrevFocusableSibling = function (elm) {
+  var nxtSibling;
+  if(!elm){
+    return false;
+  }
+  nxtSibling = iterators.getPrevSibling(elm);
+  if(nxtSibling ){
+    var focusable = iterators.getFirstFocusable(nxtSibling);
+    if(focusable){
+      return focusable;
+    }else{
+      return iterators.getPrevFocusableSibling(nxtSibling);
+    }
+  }
+  return false;
+}
+
+// get non emppty text node as next sibling
+iterators.getNextSibling = function (elm) {
+  var nxtSibling;
+  if(!elm){
+    return false;
+  }
+  nxtSibling = elm.nextSibling;
+  while (nxtSibling && nxtSibling.nodeType!=1) {
+    nxtSibling=nxtSibling.nextSibling;
+  }
+  return nxtSibling;
+}
+
+// get non emppty text node as prev sibling
+iterators.getPrevSibling = function (elm) {
+  var prevSibling;
+  if(!elm){
+    return false;
+  }
+  prevSibling = elm.previousSibling;
+  while (prevSibling && prevSibling.nodeType!=1) {
+    prevSibling=prevSibling.previousSibling;
+  }
+  return prevSibling;
+}
+
 function setFocus (elm) {
   if(!elm){
     return false;
@@ -93,170 +226,12 @@ function unsetFocus (elm) {
   elm.classList.remove('focus');
 }
 
-iterators.getUpFocusable = function (elm) {
-   if(!elm){
-    return false;
-  }else if(elm.getAttribute('data-nav') === 'top-container'){
-    return false;
+function resetFocus (focus) {
+  var lastfocus;
+  if(focus){
+    lastfocus = focusedElm;
+    focusedElm = focus;
+    unsetFocus(lastfocus);
+    setFocus(focusedElm);
   }
-
-  var parent;
-  parent = elm.parentNode;
-
-  if(parent){
-    if(parent.getAttribute('data-nav') === 'vertical-container'){
-      return iterators.getPrevFocusableSibling(elm);
-    }else if(parent.getAttribute('data-nav') === 'top-container'){
-      return iterators.getPrevFocusableSibling(elm);
-    }else{
-      return iterators.getUpFocusable(parent);
-    }
-  }
-
-  return false;
 };
-
-iterators.getDownFocusable = function (elm) {
-  if(!elm){
-    return false;
-  }else if(elm.getAttribute('data-nav') === 'top-container'){
-    return false;
-  }
-
-  var parent;
-  parent = elm.parentNode;
-
-
-  if(parent){
-    if(parent.getAttribute('data-nav') === 'vertical-container'){
-      return iterators.getNextFocusableSibling(elm);
-    }else if(parent.getAttribute('data-nav') === 'top-container'){
-      return iterators.getNextFocusableSibling(elm);
-    }else{
-      return iterators.getDownFocusable(parent);
-    }
-  }
-
-  return false;
-};
-
-iterators.getRightFocusable = function (elm) {
-  if(!elm){
-    return false;
-  }else if(elm.getAttribute('data-nav') === 'top-container'){
-    return false;
-  }
-
-  var parent=elm.parentNode;
-
-  if(parent){
-    if(parent.getAttribute('data-nav') === 'horizontal-container'){
-      return iterators.getNextFocusableSibling(elm);
-    }else if(parent.getAttribute('data-nav') === 'top-container'){
-      return iterators.getNextFocusableSibling(elm);
-    }else{
-      return iterators.getRightFocusable(parent);
-    }
-  }
-
-  return false;
-};
-
-iterators.getLeftFocusable = function (elm) {
-  if(!elm){
-    return false;
-  }else if(elm.getAttribute('data-nav') === 'top-container'){
-    return false;
-  }
-
-  var parent=elm.parentNode;
-
-  if(parent){
-    if(parent.getAttribute('data-nav') === 'horizontal-container'){
-      return iterators.getPrevFocusableSibling(elm);
-    }else if(parent.getAttribute('data-nav') === 'top-container'){
-      return iterators.getPrevFocusableSibling(elm);
-    }else{
-      return iterators.getLeftFocusable(parent);
-    }
-  }
-
-return false;
-};
-
-iterators.getFirstFocusable = function (elm) {
-  if(!elm){
-    return false;
-  }
-  if(elm && elm.getAttribute("data-nav") === 'focusable'){
-    return elm;
-  }
-
-  var all = elm.getElementsByTagName("*");
-
-  for (var i=0, max=all.length; i < max; i++) {
-    if(all[i].getAttribute("data-nav") === 'focusable'){
-      return all[i];
-    };
-
-  }
-  return false;
-};
-
-iterators.getNextFocusableSibling = function (elm) {
-  if(!elm){
-    return false;
-  }
-
-  var nxtSibling = iterators.getNextSibling(elm);
-  if(nxtSibling ){
-    var focusable = iterators.getFirstFocusable(nxtSibling);
-    if(focusable){
-      return focusable;
-    }else{
-      return iterators.getNextFocusableSibling(nxtSibling);
-    }
-  }
-  return false;
-};
-
-iterators.getPrevFocusableSibling = function (elm) {
-    if(!elm){
-    return false;
-  }
-
-  var nxtSibling = iterators.getPrevSibling(elm);
-  if(nxtSibling ){
-    var focusable = iterators.getFirstFocusable(nxtSibling);
-    if(focusable){
-      return focusable;
-    }else{
-      return iterators.getPrevFocusableSibling(nxtSibling);
-    }
-  }
-  return false;
-}
-
-// get non emppty text node as next sibling
-iterators.getNextSibling = function (elm) {
-  if(!elm){
-    return false;
-  }
-  var nxtSibling = elm.nextSibling;
-  while (nxtSibling && nxtSibling.nodeType!=1) {
-    nxtSibling=nxtSibling.nextSibling;
-  }
-  return nxtSibling;
-}
-
-// get non emppty text node as prev sibling
-iterators.getPrevSibling = function (elm) {
-  if(!elm){
-    return false;
-  }
-  var prevSibling = elm.previousSibling;
-  while (prevSibling && prevSibling.nodeType!=1) {
-    prevSibling=prevSibling.previousSibling;
-  }
-  return prevSibling;
-}
